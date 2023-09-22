c192cd5e61b75556441a65c9b00f7b22558954bf
juanmaishere <rsjuanmanuel@hotmail.com>
2023-09-22 19:01:28 +0000

    feat

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import hidden_4
+if __name__ == "__main__"
+    import hidden_4
 
-vari = dir(hidden_4)
+    names = dir(hidden_4)
 
-for v in vari:
-    if (vari[0:2] != "__"):
-        print(vari)
+    for name in names:
+        if (name[:2] != "__"):
+            print(name)
