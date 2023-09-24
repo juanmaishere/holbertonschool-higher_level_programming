6f9ef3dca03baff267de63ab711e3f0933be4072
juanmaishere <rsjuanmanuel@hotmail.com>
2023-09-24 15:30:37 +0000

    featg

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    
     if (sentence):
         lens = len(sentence)
         a = sentence[0]
         tuple = (lens, a)
         return (tuple)
-    return(None)
+    
+    tuple = (lens, None)
+    return(tuple)
