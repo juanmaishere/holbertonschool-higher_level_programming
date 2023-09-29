5214c2ba08212912d4c47e3729e591db06ac8ded
juanmaishere <rsjuanmanuel@hotmail.com>
2023-09-29 20:53:49 +0000

    feat

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""
+Definition of a class Square with pass placeholder
+"""
+
 class Square:
+    """
+    Class Square with no attributes
+    """
     pass
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+"""
+This module defines a class 'Square' with a size value.
+"""
+
 class Square:
+    """
+    This class represents a square with a given size.
+    """
+
     def __init__(self, size):
+        """
+        Initializes a new square with the specified size.
+
+        :param size: The size of the square.
+        :type size: int
+        """
         self.__size = size
