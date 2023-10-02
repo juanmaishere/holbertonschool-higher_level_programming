0eba23cdfe548442a7f8d43699a3dd5233c13f7d
juanmaishere <rsjuanmanuel@hotmail.com>
2023-10-02 17:42:43 +0000

    feat

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -19,20 +19,8 @@ class Rectangle:
         @height
         Class rectangle with width and height
         """
-        if (isinstance(width, int)):
-            if width >= 0:
-                self.__width = width
-            else:
-                raise ValueError("width must be >= 0")
-        else:
-            raise TypeError("width must be an integer")
-        if (isinstance(height, int)):
-            if height >= 0:
-                self.__height = height
-            else:
-                raise ValueError("height must be >= 0")
-        else:
-            raise TypeError("height must be an integer")
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
