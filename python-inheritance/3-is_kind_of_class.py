953de4552ba8c739a0f1cb11e8c5d95a9bfb6134
juanmaishere <rsjuanmanuel@hotmail.com>
2023-10-04 16:46:19 +0000

    feat

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -5,6 +5,9 @@ Function to check wether is the same class or
 
 
 def is_kind_of_class(obj, a_class):
+    """
+    Function checking if its an instance
+    """
     if isinstance(obj, a_class):
         return True
     else:
