a899dac39c88c115d0f9bc15f8054a091112aad7
juanmaishere <rsjuanmanuel@hotmail.com>
2023-10-04 13:39:55 +0000

    feat

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Function lookups dir of obj
+"""
+
+
+def lookup(obj):
+    """
+    :list list of dir
+    """
+    return (list(dir(obj)))
---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/bin/usr/python3
+"""
+my list class with method for printing
+"""
+
+
+class MyList(list):
+    """
+    My list class with
+    :print_sorted print all list
+    """
+    
+    def print_sorted(self):
+        self = sorted(self)
+        print(self)
