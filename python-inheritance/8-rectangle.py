f16985dd6eae44b6c67569bedcccfab4c7bf445a
juanmaishere <rsjuanmanuel@hotmail.com>
2023-10-04 17:06:12 +0000

    feat

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Declaration of A Rectangle Class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class rectangle initialized
+    """
+    def __init__(self, width, height):
+        """
+        Validation of integer for rectangle
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
