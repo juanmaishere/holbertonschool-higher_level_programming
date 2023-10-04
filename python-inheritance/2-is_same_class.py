3171fac8342f9f003eb934e7e0ed4118895aa942
juanmaishere <rsjuanmanuel@hotmail.com>
2023-10-04 14:04:41 +0000

    feat

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,2 +1,8 @@
-#!/env
+#!/usr/bin/python3
+"""
+Function checking if an object is the exact same of x class
+"""
 def is_same_class(obj, a_class):
+    """Is it the same class?"""
+    a = type(obj)
+    return a is a_class
