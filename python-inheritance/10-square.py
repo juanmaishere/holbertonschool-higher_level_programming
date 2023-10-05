82ecfe86b10d8ad99b00c349a6c3ae271edded6b
juanmaishere <rsjuanmanuel@hotmail.com>
2023-10-05 13:35:36 +0000

    feat

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -7,8 +7,15 @@ class Square(Rectangle):
     """ Square rectangle inherit class"""
 
     def __init__(self, size):
+        """
+        Init For the Square 
+        :Area Returns area
+        """
         self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
+        """
+        Area Method Func for square
+        """
         return self.__size * 2
