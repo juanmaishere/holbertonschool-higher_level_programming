91c062400225b5ac95c463c5228e8a486980e02d
juanmaishere <rsjuanmanuel@hotmail.com>
2023-10-05 14:07:23 +0000

    feat

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -2,7 +2,6 @@
 """Declaration of A Rectangle Class"""
 Rectangle = __import__('9-rectangle').Rectangle
 
-
 class Square(Rectangle):
     """ Square rectangle inherit class"""
 
@@ -19,3 +18,9 @@ class Square(Rectangle):
         Area Method Func for square
         """
         return self.__size * self.__size
+    
+    def __str__(self):
+        """
+        str returns square width and height
+        """
+        return f"[Square] {self.__size}/{self.__size}"
