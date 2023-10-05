1d94c90f579aaeefdc5c1b6e3849a00bf3c1ce55
juanmaishere <rsjuanmanuel@hotmail.com>
2023-10-05 13:20:11 +0000

    feat

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,19 +1,8 @@
-#!/usr/bin/python3
-"""Declaration of A BaseGeometry Class"""
-
-
-class BaseGeometry:
-    """ Pass Placeholder """
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        self.name = name
-        if isinstance(value, int):
-            if value > 0:
-                return self.name
-            else:
-                raise ValueError(f"{name} must be greater than 0")
-        else:
-            raise TypeError(f"{name} must be an integer")
+def integer_validator(self, name, value):
+        """function to validate an itenger"""
+        if isinstance(name, str):
+            self.name = name
+        if not isinstance(value, int):
+            raise TypeError(f"{self.name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{self.name} must be greater than 0")
