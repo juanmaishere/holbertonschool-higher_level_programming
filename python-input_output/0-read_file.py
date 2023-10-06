770435cf9c182880744b0b1a92bf84520f9b815c
root <rsjuanmanuel@hotmail.com>
2023-10-06 14:43:31 +0000

    feat

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+Summary
+"""
 
 
 def read_file(filename=""):
+    """
+    function that reads a file
+    """
     with open(filename, 'r', encoding="utf-8") as f:
         for line in f:
             print(line, end="")
