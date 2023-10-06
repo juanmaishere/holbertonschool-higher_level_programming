7cde23870d980d954db6e6fa0c25a2e250801fcf
juanmaishere <rsjuanmanuel@hotmail.com>
2023-10-06 22:21:35 +0000

    feat

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -2,10 +2,11 @@
 """summary"""
 import json
 
+
 def save_to_json_file(my_obj, filename):
     """
-    sabe an object to json and a file
+    save an object to json and a file
     """
     with open(filename, 'w', encoding="utf-8") as f:
         c = json.dumps(my_obj)
-        f.write(c)
+    f.write(c)
--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -8,4 +8,4 @@ def load_from_json_file(filename):
     load a json file
     """
     with open(filename, 'r') as f:
-        c = json.load(filename)
+        c = json.load(f)
