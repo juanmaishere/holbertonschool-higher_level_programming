d4d273dc0913b7885fe2a8207f8d124cccc9e51f
root <rsjuanmanuel@hotmail.com>
2023-10-06 14:50:50 +0000

    feat

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Summary """
+
+def write_file(filename="", text=""):
+    """ Write function that writes"""
+    with open(filename, 'w', encoding="utf-8") as f:
+        c = f.write(text)
+    return c
