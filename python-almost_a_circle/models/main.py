6d51e0f8e5d6fea3afb83a9ef470fbc09941e1d8
juanmaishere <rsjuanmanuel@hotmail.com>
2023-10-11 17:57:46 +0000

    faet

--- python-almost_a_circle/models/main.py
+++ python-almost_a_circle/models/main.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
-""" 2-main """
-from rectangle import Rectangle
+""" 12-main """
+from square import Square
 
 if __name__ == "__main__":
 
-    try:
-        Rectangle(10, "2")
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    r1 = Square(10, 2, 1, 9)
+    print(r1)
+    r1_dictionary = r1.to_dictionary()
+    print(r1_dictionary)
+    print(type(r1_dictionary))
 
-    try:
-        r = Rectangle(10, 2)
-        r.width = -10
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        r = Rectangle(10, 2)
-        r.x = {}
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        Rectangle(10, 2, 3, -1)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    r2 = Square(1, 1)
+    print(r2)
+    r2.update(**r1_dictionary)
+    print(r2)
+    print(r1 == r2)
