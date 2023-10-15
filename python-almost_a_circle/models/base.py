aeeb72a4df728c0f83252cc84bdd6d21ac601f14
juanmaishere <rsjuanmanuel@hotmail.com>
2023-10-15 15:44:49 +0000

    featg

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Summary """
+import json
 
 
 class Base:
@@ -17,3 +18,24 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        converts a list of dict to json respresentation
+        """
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        if list_objs is None:
+            list_objs = []
+        filename = f"{cls.__name__}.json"
+        
+        for obj in list_objs:
+            with open(filename, "w") as file:
+                json_str = cls.to_json_string(obj)
+                file.write(json_str)
