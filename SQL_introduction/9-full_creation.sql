3bb214c9e085d6808c97cf81f6dcc73076601572
root <juanmaishere>
2023-11-11 13:13:42 +0000

    feat

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,16 @@
+-- CREATE TABLE AND ADD MULTIPLE ROWS
+
+-- CREATE Table
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT,
+);
+
+-- ADD Multiples ROWS
+INSERT INTO second_table
+VALUES
+(id = 1, name = "John", score = 10),
+(id = 2, name = "Alex", score = 3),
+(id = 3, name = "Bob", score = 14),
+(id = 4, name = "George", score = 8);
