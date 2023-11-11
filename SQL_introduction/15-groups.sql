42acb64fe70072589b8091574f412d694273df08
juanmaishere <135636931+juanmaishere@users.noreply.github.com>
2023-11-11 16:22:04 -0300

    Update 15-groups.sql

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -3,4 +3,5 @@
 -- Lists the number of records with the same score in the table
 SELECT score, COUNT(DISTINCT score) AS number
 FROM second_table
-ORDER BY score DESC;
+GROUP BY score
+ORDER BY number DESC;
