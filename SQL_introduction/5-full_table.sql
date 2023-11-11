8a4ff3f4907e3e1041fba19c38dab024628373e0
juanmaishere <135636931+juanmaishere@users.noreply.github.com>
2023-11-11 16:35:04 -0300

    Update 5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,4 @@
 -- Display a fulll table :D
 
--- Get table structure information without using DESCRIBE or EXPLAIN
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'first_table';
+-- Get table structure information
+DESCRIBE first_table;
