1fbfafb2e7bd0ed8e7ff27bebb22f697eed381f5
juanmaishere <135636931+juanmaishere@users.noreply.github.com>
2023-11-11 16:25:12 -0300

    Create 16-no_link.sql

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+-- Write a script that lists all records of the table second_table
+
+-- DISPLAY WITH CONDITION DATABASE
+SELECT score, name FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
