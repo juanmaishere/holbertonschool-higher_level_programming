1928c6d1d3678730c89741dce3b55d4921c98251
juanmaishere <135636931+juanmaishere@users.noreply.github.com>
2023-11-14 08:32:23 -0300

    Update 8-cities_of_california_subquery.sql

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,12 +1,10 @@
 -- SUBQUERY SELECT ALL RESULTS WITH X
 
-SELECT id FROM states
-WHERE name = "California";
-
--- Select all cities in California and order by id
-SELECT name FROM cities
+-- Select all cities in California and order by cities.id
+SELECT cities.id, cities.name
+FROM cities
 WHERE state_id = (
     SELECT id FROM states
     WHERE name = "California"
 )
-ORDER BY id ASC;
+ORDER BY cities.id ASC;
