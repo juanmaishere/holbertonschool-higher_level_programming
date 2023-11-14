4ffeb6a8bd7fadb47f6dc9e6b0c2edd6de3c58af
juanmaishere <135636931+juanmaishere@users.noreply.github.com>
2023-11-14 10:49:45 -0300

    Create 9-cities_by_state_join.sql

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Write a script that lists all cities contained in the database
+
+-- List all cities by x factors
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
