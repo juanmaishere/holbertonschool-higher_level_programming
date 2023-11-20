b62c6f9d125fcbfd502c131af9314edcb664058a
juanmaishere <rsjuanmanuel@hotmail.com>
2023-11-20 10:00:13 -0300

    faet

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,18 +2,17 @@
 """
 List all states from a database sql query
 """
-from sys import argv
 import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-
     username = argv[1]
     password = argv[2]
     database = argv[3]
 
     conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
